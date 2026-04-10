52a6cbe698fd976ce6da5ffd4554f4df86d935c8
Milaa-au <mila.audu@icloud.com>
2026-04-10 12:42:40 +0000

    Ajout de la task 00

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""TASK 0"""
+
+
+def add(a: float, b: float) -> float:
+    """Fonction qui retourne la somme de a et b"""
+    return a + b
---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""TASK 1"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Retourne STR 1 ET STR 2 mais concaté."""
+    return str1 + str2
